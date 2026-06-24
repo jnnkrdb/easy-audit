bcbddfc29940d04b9fe83dcd926516cdb0e5442d
jnnkrdb <jannek.rodenburger@icloud.com>
2026-06-24 15:21:37 +0000

    added check if there are any health functions, if not, skips for loop

--- pkg/http/probez/healthzcheck.go
+++ pkg/http/probez/healthzcheck.go
@@ -9,11 +9,13 @@ type Probe func() (bool, error)
 
 func HandleHTTP(fn ...Probe) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		for _, probe := range fn {
-			if ok, err := probe(); !ok || err != nil {
-				w.WriteHeader(http.StatusServiceUnavailable)
-				fmt.Fprintf(w, "not ok")
-				return
+		if len(fn) > 0 {
+			for _, probe := range fn {
+				if ok, err := probe(); !ok || err != nil {
+					w.WriteHeader(http.StatusServiceUnavailable)
+					fmt.Fprintf(w, "not ok")
+					return
+				}
 			}
 		}
 		w.WriteHeader(http.StatusOK)
